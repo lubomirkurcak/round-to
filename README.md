424ea17c425c3f156dff46fbcfb2257c49139a8d
Ľubomír Kurčák <lubomirkurcak@gmail.com>
2023-10-07 19:34:10 +0200

    rename repo

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
-# round_to_int
+# round-to
 Round floating point to integer.
 
 ### Usage
 You can round to `i32` and `i64` explicitly:
 ```rust
-use round_to_int::*;
+use round_to::*;
 
 assert_eq!(0.4.round_to_i32(), 0);
 assert_eq!(0.5.round_to_i64(), 1);
 ```
 or implicitly to `i8`, `i16`, `i32`, `i64`, `i128`, `isize`, `u8`, `u16`, `u32`, `u64`, `u128`, or `usize`:
 ```rust
-use round_to_int::*;
+use round_to::*;
 
 let a: i8 = 0.4.round_to();
 assert_eq!(a, 0);
 ```
 using these modes:
 ```rust
-use round_to_int::*;
+use round_to::*;
 
 assert_eq!(0.5.round_to_i32(), 1);
 assert_eq!(0.5.floor_to_i32(), 0);
--- src/floor.rs
+++ src/floor.rs
@@ -3,14 +3,14 @@
 /// # Usage
 /// You can floor to `i32` and `i64` explicitly:
 /// ```rust
-/// use round_to_int::*;
+/// use round_to::*;
 ///
 /// assert_eq!(0.7.floor_to_i32(), 0);
 /// assert_eq!(1.3.floor_to_i64(), 1);
 /// ```
 /// or implicitly to `i8`, `i16`, `i32`, `i64`, `i128`, `isize`, `u8`, `u16`, `u32`, `u64`, `u128`, or `usize`:
 /// ```rust
-/// use round_to_int::*;
+/// use round_to::*;
 ///
 /// let a: i8 = 0.4.floor_to();
 /// assert_eq!(a, 0);
@@ -21,14 +21,14 @@ pub trait FloorTo<T> {
     /// # Usage
     /// You can floor to `i32` and `i64` explicitly:
     /// ```rust
-    /// use round_to_int::*;
+    /// use round_to::*;
     ///
     /// assert_eq!(0.7.floor_to_i32(), 0);
     /// assert_eq!(1.3.floor_to_i64(), 1);
     /// ```
     /// or implicitly to `i8`, `i16`, `i32`, `i64`, `i128`, `isize`, `u8`, `u16`, `u32`, `u64`, `u128`, or `usize`:
     /// ```rust
-    /// use round_to_int::*;
+    /// use round_to::*;
     ///
     /// let a: i8 = 0.4.floor_to();
     /// assert_eq!(a, 0);
@@ -60,14 +60,14 @@ macro_rules! floor_to_explicit {
         /// # Usage
         /// You can floor to `i32` and `i64` explicitly:
         /// ```rust
-        /// use round_to_int::*;
+        /// use round_to::*;
         ///
         /// assert_eq!(0.7.floor_to_i32(), 0);
         /// assert_eq!(1.3.floor_to_i64(), 1);
         /// ```
         /// or implicitly to `i8`, `i16`, `i32`, `i64`, `i128`, `isize`, `u8`, `u16`, `u32`, `u64`, `u128`, or `usize`:
         /// ```rust
-        /// use round_to_int::*;
+        /// use round_to::*;
         ///
         /// let a: i8 = 0.4.floor_to();
         /// assert_eq!(a, 0);
@@ -78,14 +78,14 @@ macro_rules! floor_to_explicit {
             /// # Usage
             /// You can floor to `i32` and `i64` explicitly:
             /// ```rust
-            /// use round_to_int::*;
+            /// use round_to::*;
             ///
             /// assert_eq!(0.7.floor_to_i32(), 0);
             /// assert_eq!(1.3.floor_to_i64(), 1);
             /// ```
             /// or implicitly to `i8`, `i16`, `i32`, `i64`, `i128`, `isize`, `u8`, `u16`, `u32`, `u64`, `u128`, or `usize`:
             /// ```rust
-            /// use round_to_int::*;
+            /// use round_to::*;
             ///
             /// let a: i8 = 0.4.floor_to();
             /// assert_eq!(a, 0);
